db61fc7260e61b20dd4985629bfce2ea794f0809
Thomas OGrady <thomasog98>
2023-11-17 20:40:55 +0000

    Better logging, error handling and Button click

--- glasto_crawl/glastonbury_botnet.py
+++ glasto_crawl/glastonbury_botnet.py
@@ -2,16 +2,20 @@
 # Filename: glastonbury_botnet.py
 import csv
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 import glastonbury as glasto
 import time
 import os
+from datetime import datetime
 
-glastonbury_url="https://glastonbury.seetickets.com/content/extras"
+
+#glastonbury_url="https://glastonbury.seetickets.com/content/extras"
 #glastonbury_url="https://glastonbury.seetickets.com/event/glastonbury-2024/worthy-farm/2400000"
 #glastonbury_url="https://glastonbury.seetickets.com/event/glastonbury-2024-deposits/worthy-farm/2400000"
-#glastonbury_url="https://glastonbury.seetickets.com/event/glastonbury-2024-deposits/worthy-farm/3500000"
+glastonbury_url="https://glastonbury.seetickets.com/event/glastonbury-2024-deposits/worthy-farm/3500000"
 #glastonbury_url="http://127.0.0.1:8080/"
+
 refresh_phrases = ["You will be held at this page until there is a free space on the booking site"]
 kill_phrases = ["Registration Number:","Postcode:"]
 path = r"C:\chromedriver.exe"
@@ -32,45 +36,96 @@ for row in csvfile:
     cnt+=1
 
 def populate_form():
-    print("Population form")
-    page_cnt=0
+
     try: 
+
+        time_date = datetime.now()
+        print(time_date,"Populating form with registration details\n")
+        page_cnt=0
         for reg_cnt in reg_details:
             browser.find_element(By.ID, "registrations_"+str(page_cnt)+"__RegistrationId").send_keys(reg_details[reg_cnt][0])
             browser.find_element(By.ID, "registrations_"+str(page_cnt)+"__PostCode").send_keys(reg_details[reg_cnt][1])
             page_cnt+=1
+
         #Submit after populating
-        browser.find_element(By.ID, "add_registrations").click()
-    except:
-        print("Issue posting")
+        time_date = datetime.now()
+        print(time_date,"Submitting registration details\n")
+        add_reg = browser.find_element(By.ID, "add_registrations")
+        browser.execute_script("arguments[0].scrollIntoView();", add_reg)
+        browser.execute_script("arguments[0].click();", add_reg)
+       # browser.find_element(By.ID, "add_registrations").Click()
+        time_date = datetime.now()
+        print(time_date,"Sucessfully added reg details\n")
+
+    except Exception as e:
+        print("Issue posting, sleeping for a while",e)
         time.sleep(1000000)
     #Prevent the script from exiting while paying for the stuff
+    time_date = datetime.now()
+    print(time_date,"Please now enter your card details to pay, entering sleep timer\n")
     time.sleep(1000000)
     time.sleep(1000000)
 
-celenium_srv = glasto.srv(path)
-browser = webdriver.Chrome(service = celenium_srv.chrome_services,options=celenium_srv.chrome_options)
-print("Welcome to Glastonbury Botnet for 2023")
+print("Welcome to Glastonbury Botnet for 2024")
+print("If you need to kill the refreshes, place kill in C directory\n")
+print("Please ensure you are using the correct chromedriver/Chrome version, Good Luck!\n")
+
 try:
+    try:
+        celenium_srv = glasto.srv(path)
+        browser = webdriver.Chrome(service = celenium_srv.chrome_services,options=celenium_srv.chrome_options)
+        
+    except Exception as ex:
+        print("Unable to start Chromedriver application, exiting application in 10 ", ex)
+        time.sleep(10)
+
     browser.get(glastonbury_url)
+    time_date = datetime.now()
+
+    print("\n"+str(time_date)+" Browser connection established")
 except:
-    print("Retrying url")
+    time_date = datetime.now()
+    print("\n"+str(time_date)+" Failed to connect to website")
+    print("\n"+str(time_date)+" Retrying url")
     try:
         browser.get(glastonbury_url)
+        time_date = datetime.now()
+        print("\n"+str(time_date)+" Browser connection established")
     except:
-        print("failed to connect to website")
-
+        try:
+            time_date = datetime.now()
+            print("\n"+str(time_date)+" Failed to connect to website")
+            print("\n"+str(time_date)+" Retrying url #2")
+            browser.get(glastonbury_url)
+            time_date = datetime.now()
+            print("\n"+str(time_date)+" Browser connection established")
+        except:
+            try:
+                time_date = datetime.now()
+                print("\n"+str(time_date)+" Failed to connect to website")
+                print("\n"+str(time_date)+" Retrying url #3")
+                browser.get(glastonbury_url)
+                time_date = datetime.now()
+                print("\n"+str(time_date)+" Browser connection established")
+            except:
+                time_date = datetime.now()
+                print("\n"+str(time_date)+" Failed to connect to website, exiting in 5 seconds \n")
+                time.sleep(5)
+                exit()
 
 while True:
     page = browser.page_source
     for phrase in kill_phrases:
         if (phrase in page):
-            print("You should now be on the booking site at this point")
+            time_date = datetime.now()
+            print("\n"+str(time_date)+" You should now be on the booking site at this point\n")
+
             populate_form()
             break  
         else:
             time.sleep(refresh_rate)
-            print("Refreshing...")
+            time_date = datetime.now()
+            print(time_date,"Refreshing...")
             if(os.path.isfile(r"C:\kill")):
                 print("Killing auto refresh")
                 break
